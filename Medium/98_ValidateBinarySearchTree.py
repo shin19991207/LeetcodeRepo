dc7a6d663db48fb1ec964a6ecd7741aef8dfc0f2
Morgan Chang <shin19991207@gmail.com>
2021-12-11 23:47:25 -0500

    Create 98_ValidateBinarySearchTree.py

---
+++ Medium/98_ValidateBinarySearchTree.py
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def helper(root, low, high):
+            if root is None:
+                return True
+            if root.val <= low or root.val >= high:
+                return False
+            return helper(root.left, low, root.val) and helper(root.right, root.val, high)
+        return helper(root, -math.inf, math.inf)
